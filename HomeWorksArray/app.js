a38bd4c76f58053475ff331f62772ebf6e7b6fcc
Alex <oleksejbogomolov@gmail.com>
2025-08-03 00:45:52 +0300

    HomeWork

---
+++ HomeWorksArray/app.js
@@ -0,0 +1,10 @@
+let user = parseInt(prompt('iСкільки елементів хочете добавити до масиву'));
+let array = [];
+for(let i = 0;  i < user; i++){
+    let elements = parseInt(prompt(`input your elements ${i + 1}:`));
+    array.push(elements);
+}
+    array.sort ((a , b) => a - b);
+    array.splice(1, 3);
+        console.log(array); 
+//        
